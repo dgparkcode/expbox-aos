e26b71a6c6eda0f006f1837b728081a3f7253a18
dgparkcode <dgpark.code@gmail.com>
2022-12-10 21:55:56 +0900

    refactor: MainActivity를 ui 패키지로 이동

--- app/src/main/java/com/dgparkcode/expbox/ui/MainActivity.kt
+++ app/src/main/java/com/dgparkcode/expbox/ui/MainActivity.kt
@@ -1,7 +1,12 @@
-package com.dgparkcode.expbox
+/*
+ * Copyright (c) 2022. dgparkcode. All rights reserved.
+ */
+
+package com.dgparkcode.expbox.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.dgparkcode.expbox.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
