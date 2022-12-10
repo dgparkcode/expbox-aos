23483f3138e22989073f4dc6c1e02211f1230e01
dgparkcode <dgpark.code@gmail.com>
2022-12-10 21:57:21 +0900

    refactor: App 클래스 추가 및 HiltAndroidApp 어노테이션 설정

---
+++ app/src/main/java/com/dgparkcode/expbox/App.kt
@@ -0,0 +1,11 @@
+/*
+ * Copyright (c) 2022. dgparkcode. All rights reserved.
+ */
+
+package com.dgparkcode.expbox
+
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App : Application()
