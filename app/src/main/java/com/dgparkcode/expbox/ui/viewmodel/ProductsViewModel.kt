e4fafdd766b4c39ee2ed25ea4fd44a3c80cdc4e8
dgparkcode <dgpark.code@gmail.com>
2022-12-13 02:16:27 +0900

    test: 테스트용 이미지 추가

--- app/src/main/java/com/dgparkcode/expbox/ui/viewmodel/ProductsViewModel.kt
+++ app/src/main/java/com/dgparkcode/expbox/ui/viewmodel/ProductsViewModel.kt
@@ -18,6 +18,7 @@ import kotlinx.coroutines.flow.update
 import kotlinx.coroutines.launch
 import java.time.LocalDate
 import javax.inject.Inject
+import kotlin.random.Random
 
 @HiltViewModel
 class ProductsViewModel @Inject constructor(
@@ -33,11 +34,19 @@ class ProductsViewModel @Inject constructor(
 
             delay(3000L)
 
+            val imageUrls = listOf(
+                "https://cdn.pixabay.com/photo/2022/12/04/10/13/snow-7634083_640.jpg",
+                "https://cdn.pixabay.com/photo/2022/11/20/09/58/leaves-7603946__340.jpg",
+                "https://cdn.pixabay.com/photo/2022/11/19/18/45/gray-geese-7602847__340.jpg",
+                "https://cdn.pixabay.com/photo/2022/12/06/06/21/lavender-7638368_640.jpg",
+                "https://cdn.pixabay.com/photo/2022/12/02/18/37/middle-spotted-woodpecker-7631440_640.jpg",
+                "https://cdn.pixabay.com/photo/2022/12/04/23/12/siblings-7635490__340.jpg",
+            )
             val fakeItems = (0..20).map {
                 val product = Product(
                     id = it.toLong(),
                     name = "product name $it",
-                    image = "",
+                    image = imageUrls[Random.nextInt(imageUrls.size)],
                     expireAt = LocalDate.now()
                 )
                 ProductItemState(product)
